0278a38a5544fe27b84f769acc1c106493c7edde
Mian Muhammad Sharjeel Safdar <miansharjeelsafdar@gmail.com>
2021-06-14 02:30:10 +0500

    Added a user pool, client, domain, dynamodb table, and graphql appsync api

--- lib/project13a-backend-stack.ts
+++ lib/project13a-backend-stack.ts
@@ -7,6 +7,93 @@ export class Project13ABackendStack extends cdk.Stack {
   constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    const todosTable = new ddb.Table(this, "TodosTable", {
+      partitionKey: {
+        name: "id",
+        type: ddb.AttributeType.STRING,
+      },
+      billingMode: ddb.BillingMode.PAY_PER_REQUEST,
+      removalPolicy: cdk.RemovalPolicy.DESTROY,
+    });
+
+    const userPool = new cognito.UserPool(this, "P13aUserPool", {
+      removalPolicy: cdk.RemovalPolicy.DESTROY,
+      selfSignUpEnabled: true,
+      signInAliases: {
+        email: true,
+        username: true,
+      },
+      signInCaseSensitive: true,
+      passwordPolicy: {
+        minLength: 8,
+        requireDigits: true,
+        requireLowercase: true,
+        requireUppercase: true,
+      },
+      autoVerify: {
+        email: true,
+      },
+      userVerification: {
+        emailStyle: cognito.VerificationEmailStyle.LINK,
+      },
+      emailSettings: {
+        from: "miansharjeelsafdar@gmail.com",
+      },
+      accountRecovery: cognito.AccountRecovery.EMAIL_ONLY,
+      standardAttributes: {
+        email: {
+          required: true,
+          mutable: false,
+        },
+      },
+    });
+
+    const userPoolClient = userPool.addClient("P13aUserPoolClient", {
+      authFlows: {
+        userPassword: true,
+      },
+      // oAuth: {
+
+      // }
+    });
+
+    const domain = userPool.addDomain("P13aUserPoolDomain", {
+      cognitoDomain: {
+        domainPrefix: "todo-app-p13a",
+      },
+    });
+
+    const gqlApi = new appsync.GraphqlApi(this, "GraphQlApi", {
+      name: "GraphQL-Api",
+      schema: appsync.Schema.fromAsset("graphql/schema.gql"),
+      authorizationConfig: {
+        defaultAuthorization: {
+          authorizationType: appsync.AuthorizationType.USER_POOL,
+          userPoolConfig: {
+            userPool,
+          },
+        },
+      },
+    });
+
+    const ddbDataSource = gqlApi.addDynamoDbDataSource(
+      "DdbDataSource",
+      todosTable
+    );
+    todosTable.grantReadWriteData(ddbDataSource);
+
+    new cdk.CfnOutput(this, "P13aUserPoolsWebClientId", {
+      value: userPoolClient.userPoolClientId,
+    });
+    new cdk.CfnOutput(this, "p13a_project_region", {
+      value: this.region,
+    });
+    new cdk.CfnOutput(this, "p13a_user_pools_id", {
+      value: userPool.userPoolId,
+    });
+
+    new cdk.CfnOutput(this, "domain", {
+      value: domain.domainName,
+    });
   }
 }
