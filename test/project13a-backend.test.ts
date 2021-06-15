9aabfed0dd234792e50dd601de4cc91469b80983
Mian Muhammad Sharjeel Safdar <miansharjeelsafdar@gmail.com>
2021-06-15 15:18:48 +0500

    Added tests for existance of resources

--- test/project13a-backend.test.ts
+++ test/project13a-backend.test.ts
@@ -1,25 +1,62 @@
-import {
-  expect as expectCDK,
-  matchTemplate,
-  MatchStyle,
-} from "@aws-cdk/assert";
+import { expect as expectCDK, haveResource } from "@aws-cdk/assert";
 import * as cdk from "@aws-cdk/core";
-import * as Project13ATodoAppBackendWithAwsCdk from "../lib/project13a-backend-stack";
+import * as Project13ABackend from "../lib/project13a-backend-stack";
 
-test("Empty Stack", () => {
+test("Stack has a Cognito User Pool", () => {
   const app = new cdk.App();
   // WHEN
-  const stack = new Project13ATodoAppBackendWithAwsCdk.Project13ABackendStack(
+  const stack = new Project13ABackend.Project13ABackendStack(
+    app,
+    "MyTestStack"
+  );
+  // THEN
+  expectCDK(stack).to(haveResource("AWS::Cognito::UserPool"));
+});
+
+test("Stack has a DynamoDB Table", () => {
+  const app = new cdk.App();
+  // WHEN
+  const stack = new Project13ABackend.Project13ABackendStack(
+    app,
+    "MyTestStack"
+  );
+  // THEN
+  expectCDK(stack).to(haveResource("AWS::DynamoDB::Table"));
+});
+
+test("Stack has a User Pool Web Client", () => {
+  const app = new cdk.App();
+  // WHEN
+  const stack = new Project13ABackend.Project13ABackendStack(
+    app,
+    "MyTestStack"
+  );
+  // THEN
+  expectCDK(stack).to(haveResource("AWS::Cognito::UserPoolClient"));
+});
+
+test("Stack has an AppSync GraphQL API", () => {
+  const app = new cdk.App();
+  // WHEN
+  const stack = new Project13ABackend.Project13ABackendStack(
+    app,
+    "MyTestStack"
+  );
+  // THEN
+  expectCDK(stack).to(haveResource("AWS::AppSync::GraphQLApi"));
+});
+
+test("GraphQL API has a DynamoDB Data Source", () => {
+  const app = new cdk.App();
+  // WHEN
+  const stack = new Project13ABackend.Project13ABackendStack(
     app,
     "MyTestStack"
   );
   // THEN
   expectCDK(stack).to(
-    matchTemplate(
-      {
-        Resources: {},
-      },
-      MatchStyle.EXACT
-    )
+    haveResource("AWS::AppSync::DataSource", {
+      Type: "AMAZON_DYNAMODB",
+    })
   );
 });
